c35219dffed5e9924d974b2984179a862a0fc0a7
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2021-06-24 16:55:18 +0200

    Local storage

--- script.js
+++ script.js
@@ -76,12 +76,15 @@ class App {
   #workouts = [];
 	
   constructor() {
+	  //Get user's position
 	  this._getPosition();
 	  
-	  form.addEventListener('submit', this._newWorkout.bind(this));
-
-	  inputType.addEventListener('change', this._toggleElevationField);
+	  //Get data from local storage
+      this._getLocalStorage();
 	  
+	  //Attach event handlers
+	  form.addEventListener('submit', this._newWorkout.bind(this));
+	  inputType.addEventListener('change', this._toggleElevationField); 
 	  containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
 
@@ -109,6 +112,24 @@ class App {
       }).addTo(this.#map);
 
       this.#map.on('click', this._showForm.bind(this));
+	  
+	  this.#workouts.forEach(work => {
+      L.marker(work.coords)
+        .addTo(this.#map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: `${work.type}-popup`,
+          })
+        )
+        .setPopupContent(
+          `${work.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${work.description}`
+        )
+        .openPopup();
+    });
   }
 
   _showForm(mapE) {
@@ -198,6 +219,9 @@ class App {
 	  
 	//Hide form + clear input fields
 	this._hideForm();
+	
+	//Set local storage to all workout
+    this._setLocalStorage();
   }
   
   _renderWorkout(workout) {
@@ -268,6 +292,26 @@ class App {
 			duration: 1,
 		}
 	});
+  }
+  
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+  
+  _getLocalStorage() {
+	const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!data) return;
+
+    this.#workouts = data;
+	
+    this.#workouts.forEach(work => this._renderWorkout(work));
+  }
+  
+  //using this in console
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
   }
 }
 
