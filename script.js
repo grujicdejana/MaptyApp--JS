97cd1f5fc5f6b9ec6ba216181008281300823e69
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2021-06-24 15:25:18 +0200

    Move to marker on click

--- script.js
+++ script.js
@@ -72,6 +72,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 class App {
   #map;
   #mapEvent;
+  #mapZoomLevel = 13;
   #workouts = [];
 	
   constructor() {
@@ -80,6 +81,8 @@ class App {
 	  form.addEventListener('submit', this._newWorkout.bind(this));
 
 	  inputType.addEventListener('change', this._toggleElevationField);
+	  
+	  containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
 
   _getPosition() {
@@ -98,7 +101,7 @@ class App {
 		
 		const coords = [latitude, longitude];
 		
-		this.#map = L.map('map').setView(coords, 13);
+		this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
@@ -246,6 +249,25 @@ class App {
       `;
 
     form.insertAdjacentHTML('afterend', html);
+  }
+  
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+    //console.log(workoutEl);
+
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+    //console.log(workout);
+	
+	this.#map.setView(workout.coords, this.#mapZoomLevel, {
+		animate: true,
+		pan: {
+			duration: 1,
+		}
+	});
   }
 }
 
